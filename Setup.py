af283c25fb591edbe7109dcfc8bfd90e5e92003e
Inan <107860167+Abdul-Hakim-Sabuj@users.noreply.github.com>
2022-10-31 17:00:18 +0600

    Add files via upload

---
+++ Setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.13'
+DESCRIPTION = 'Extra Mathmatics for python'
+LONG_DESCRIPTION = 'A package that allows to do Extra Mathmatics in python.'
+
+# Setting up
+setup(
+    name="PyMath",
+    version=VERSION,
+    author="Inan",
+    author_email="<abdulhakim.sabuj@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'Math', 'Mathmatics'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
